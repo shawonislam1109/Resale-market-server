0a3e214b566fd3b682c2bb1e8c5cb7ede330b690
shawonislam1109 <shawonislam1109@gmail.com>
2022-11-24 18:34:45 +0600

    mongodb and product collection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,11 +11,39 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const user = process.env.BD_User
+const password = process.env.BD_Password
+
+
+
 app.get('/', (req, res) => {
     res.send({
         message: 'this server is connected'
     })
 })
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.5rnuhbi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const ProductCollection = client.db('Resale-market').collection('allProduct')
+
+
+        app.get('/allProducts', async (req, res) => {
+            const query = {};
+            const products = await ProductCollection.find(query).toArray()
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
+
+
 
 
 app.listen(port, () => {
